1b8537db97336e84610d863b7e56b11eea123780
Sabbir-663 <ahamedsabbir663@gmail.com>
2025-07-07 14:24:06 +0600

    kfjak

--- dist/app.js
+++ dist/app.js
@@ -3,12 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const book_controller_1 = require("./app/controllers/book.controller");
 const borrow_controller_1 = require("./app/controllers/borrow.controller");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 // const router = express.Router();
+app.use((0, cors_1.default)({
+    origin: ['http://localhost:5173', 'https://assignment-4-ruby-gamma.vercel.app'], // allow frontend dev server
+    credentials: true // optional, only needed if you're using cookies or headers like Authorization
+}));
 app.use('/api', book_controller_1.booksRouter);
 app.use('/api', borrow_controller_1.borrowsRouter);
 app.get('/', (req, res, next) => {
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 
+import cors from "cors";
 import express, { Application, NextFunction, Request, Response }  from 'express'
 import { booksRouter } from './app/controllers/book.controller';
 import { borrowsRouter } from './app/controllers/borrow.controller';
@@ -6,7 +7,10 @@ const app:Application = express()
 app.use(express.json())
 
 // const router = express.Router();
-
+app.use(cors({
+  origin: ['http://localhost:5173','https://assignment-4-ruby-gamma.vercel.app'],  // allow frontend dev server
+  credentials: true               // optional, only needed if you're using cookies or headers like Authorization
+}));
 app.use('/api',booksRouter)
 app.use('/api',borrowsRouter)
 
